e1990ae0e7a920a87d034fc2ba77afebbf0ac51f
Soham <nimbalkarss123@gmail.com>
2024-03-11 19:53:25 +0530

    training added

--- src/cnnClassifier/components/prepare_callbacks.py
+++ src/cnnClassifier/components/prepare_callbacks.py
@@ -10,13 +10,12 @@ class PrepareCallback:
     def __init__(self, config: PrepareCallbacksConfig):
         self.config = config
 
-
     
     @property
     def _create_tb_callbacks(self):
         timestamp = time.strftime("%Y-%m-%d-%H-%M-%S")
         tb_running_log_dir = os.path.join(
-            self.config.tensorboard_root_log_dir,
+            str(self.config.tensorboard_root_log_dir),  # Convert Path to string
             f"tb_logs_at_{timestamp}",
         )
         return tf.keras.callbacks.TensorBoard(log_dir=tb_running_log_dir)
@@ -25,7 +24,7 @@ class PrepareCallback:
     @property
     def _create_ckpt_callbacks(self):
         return tf.keras.callbacks.ModelCheckpoint(
-            filepath=self.config.checkpoint_model_filepath,
+            filepath=str(self.config.checkpoint_model_filepath),  # Convert Path to string
             save_best_only=True
         )
 
